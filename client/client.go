6840552f6830e758b59b48649deca31ec5f52acb
liaoshengchen <liaoshengchen@baidu.com>
2023-05-08 19:18:25 +0800

    v0.4

--- client/client.go
+++ client/client.go
@@ -5,16 +5,21 @@ import (
 	"github.com/go-resty/resty/v2"
 	"log"
 	"math"
+	"sync"
 	"time"
 )
 
 var (
 	client       *resty.Client
-	tickInterval time.Duration = 15 * time.Second
+	tickInterval time.Duration = 1 * time.Second
 	sleepTime    time.Duration = 5 * time.Minute
+	A                          = 30
+	lock         sync.Mutex
+	statistic    map[string]int
 )
 
 func send() {
+	statistic = make(map[string]int)
 	client = resty.New()
 	ticker := time.NewTicker(tickInterval)
 	i := 0
@@ -22,6 +27,7 @@ func send() {
 	go func() {
 		time.Sleep(sleepTime)
 		ticker.Stop()
+		fmt.Println(statistic)
 		close(done)
 	}()
 	for {
@@ -30,7 +36,7 @@ func send() {
 			log.Println("over")
 			return
 		case <-ticker.C:
-			lauch(Sin(int(sleepTime.Seconds()/tickInterval.Seconds()), i, 30))
+			lauch(Sin(int(sleepTime.Seconds()/tickInterval.Seconds()), i, A))
 			i++
 
 		}
@@ -42,14 +48,34 @@ func lauch(i int) {
 		go func() {
 			rsp, err := client.R().
 				EnableTrace().
-				Get("http://127.0.0.1:43787/random")
+				Get("http://127.0.0.1:51451/random")
 			if err != nil {
 				log.Println(err)
+				return
 			}
-			fmt.Println(rsp.String())
+			lock.Lock()
+			statistic[rsp.Status()]++
+			lock.Unlock()
+			//fmt.Println(rsp.String())
 		}()
 	}
 }
 func Sin(n int, i int, A int) int {
-	return A * int(math.Sin(2*math.Pi/float64(n*i))+1)
+	return int(float64(A) * (math.Sin(2*math.Pi/float64(n)*float64(i)) + 1))
+}
+func ShowWave() string {
+	n := int(sleepTime.Seconds() / tickInterval.Seconds())
+	res := make([]int, 0)
+	for i := 0; i < n; i++ {
+		res = append(res, Sin(n, i, A))
+	}
+	str := ""
+	for i, v := range res {
+		if i == len(res)-1 {
+			str += fmt.Sprintf("%d", v)
+			break
+		}
+		str += fmt.Sprintf("%d,", v)
+	}
+	return fmt.Sprintf("[%s]", str)
 }
--- client/client_test.go
+++ client/client_test.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"sync"
 	"testing"
+	"time"
 )
 
 func TestSend(t *testing.T) {
@@ -21,21 +22,28 @@ func TestSend(t *testing.T) {
 	//}
 	//fmt.Println(string(rsp.Body()))
 }
+func TestShowWave(t *testing.T) {
+	res := ShowWave()
+	fmt.Println(res)
+}
 func TestLocal(t *testing.T) {
 	client = resty.New()
 	//client.R().
 	wg := sync.WaitGroup{}
-	for i := 0; i < 21; i++ {
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-			rsp, err := client.R().Get("http://127.0.0.1:8080/random")
-			if err != nil {
-				log.Println(err)
-			}
-			fmt.Println(i, " : ", rsp.String())
-		}(i)
+	for j := 0; j < 3; j++ {
+		for i := 0; i < 21; i++ {
+			wg.Add(1)
+			go func(i int) {
+				defer wg.Done()
+				rsp, err := client.R().Get("http://127.0.0.1:50821/random")
+				if err != nil {
+					log.Println(err)
+				}
+				fmt.Println(i, " : ", rsp.String())
+			}(i)
+		}
+		wg.Wait()
+		time.Sleep(15 * time.Second)
 	}
-	wg.Wait()
 
 }
